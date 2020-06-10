7d95088b9b89e1445b9ec9f91e2bd8d7c2ed7239
IgorCrepo <Skilledea@gmail.com>
2020-06-10 18:58:08 +0200

    ProjektPZ

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn crm.wsgi.py --log-file -
+web: gunicorn crm.wsgi --log-file -
--- crm/settings.py
+++ crm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'a+etpz#ocp)#54le&e$^g7*g_f^hkvs#t1er-$#98e#%-*tziw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['projektpz.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['projektpz.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -126,7 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
